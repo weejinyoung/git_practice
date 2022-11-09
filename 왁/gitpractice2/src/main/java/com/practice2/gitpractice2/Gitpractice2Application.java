041e01ed848c26b83d85734b7692fe7a2c992cd6
Jn W <a8118199@gmail.com>
2022-11-09 10:18:08 +0900

    FEAT : 연습용 프로젝트 2 생성

---
+++ 왁/gitpractice2/src/main/java/com/practice2/gitpractice2/Gitpractice2Application.java
@@ -0,0 +1,13 @@
+package com.practice2.gitpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitpractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitpractice2Application.class, args);
+	}
+
+}
