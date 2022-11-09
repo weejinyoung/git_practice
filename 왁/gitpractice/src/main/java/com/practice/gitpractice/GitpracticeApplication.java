5744438f9b99690e753e57b9aaae2159f867a6ef
Jn W <a8118199@gmail.com>
2022-11-09 10:15:29 +0900

    FEAT : 연습용 프로젝트 생성

---
+++ 왁/gitpractice/src/main/java/com/practice/gitpractice/GitpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.gitpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpracticeApplication.class, args);
+	}
+
+}
